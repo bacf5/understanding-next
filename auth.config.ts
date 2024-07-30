f902c92e5c4ccedb5f478f164ce1f49a3b958083
bacf <brunoagustincarusofassa@gmail.com>
2024-07-30 20:05:32 -0300

    Chapter 15: Adding the sign in functionality

--- auth.config.ts
+++ auth.config.ts
@@ -4,4 +4,19 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-};
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // redirects to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // add providers wtih empty array for now
+} satisfies NextAuthConfig;
